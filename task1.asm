56370a3dc0c7d4b6584cb63d0cd1ec02bebfc8c6
TonyDiaz03 <112600770+TonyDiaz03@users.noreply.github.com>
2023-03-27 20:28:11 -0700

    task 2 version

--- task1.asm
+++ task1.asm
@@ -3,6 +3,11 @@ input1: .asciiz "Enter the first integer: "
 input2: .asciiz "Enter the second integer: "
 result: .asciiz "You entered: "
 andString: .asciiz " and "
+addResult: .asciiz "\nThe addition of the integers is "
+subResult: .asciiz "\nThe subtraction of the integers is "
+mulResult: .asciiz "\nThe multiplication of the integers is "
+divResult: .asciiz "\nThe division of the integers is "
+divRemainder: .asciiz " with the remainder of "
     
 .text
 main:
@@ -47,11 +52,65 @@ main:
     syscall
     
     #adding the 2 values 
-    add $s2, $s1, $s0
+    add $s2, $s0, $s1
+    
+    #subtracting the values
+    sub $s3, $s0, $s1 
+    
+    #multiplying the values
+    mul $s4, $s0, $s1
+    
+    #dividing the values
+    div $s5, $s0, $s1
+    
+    #print the add statement
+    li $v0, 4
+    la $a0, addResult
+    syscall
     
     #printing the added result
     li $v0, 1
     move $a0, $s2
+    syscall
+    
+    #printing the subtract statement
+    li $v0, 4
+    la $a0, subResult
+    syscall
+    
+    #printing the subtracted result
+    li $v0, 1
+    move $a0, $s3
+    syscall
+    
+    #printing the multiplied statement
+    li $v0, 4
+    la $a0, mulResult
+    syscall
+    
+    #printing the multiplied result
+    li $v0, 1
+    move $a0, $s4
+    syscall
+    
+    #printing the divided statement
+    li $v0, 4
+    la $a0, divResult
+    syscall
+    
+    #printing the divided result
+    li $v0, 1
+    mflo $a0
+    syscall
+    
+    #printing the remainder statement
+    li $v0, 4
+    la $a0, divRemainder 
+    syscall
+    
+    #printingthe remainder result
+    li $v0, 1
+    mfhi $a0
     syscall
     
     # Exit program
